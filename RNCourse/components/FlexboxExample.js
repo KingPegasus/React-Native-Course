46bf081beab767995cbe5efa7698ea88e36d3381
Raza Saleemi <rks143@gmail.com>
2025-08-09 08:04:50 +0500

    ScrollView

--- RNCourse/components/FlexboxExample.js
+++ RNCourse/components/FlexboxExample.js
@@ -1,44 +1,44 @@
-
 import { useState } from 'react';
-import { StyleSheet, Text, TextInput, View, Button } from 'react-native';
+import { StyleSheet, Text, TextInput, View, Button, ScrollView } from 'react-native';
 
 const FlexboxExample = () => {
   const [enteredGoalText, setEnteredGoalText] = useState('');
   const [courseGoals, setCourseGoals] = useState([]);
   function goalInputHandler(enteredText) {
     setEnteredGoalText(enteredText);
-  };
+  }
   function addGoalHandler() {
-    setCourseGoals((currentGoals) => [...currentGoals, enteredGoalText]);
+    setCourseGoals(currentGoals => [...currentGoals, enteredGoalText]);
     setEnteredGoalText('');
-  };
-
+  }
 
   return (
-    <View style={{flex: 1}}>
+    <View style={{ flex: 1 }}>
       <View style={styles.inputContainer}>
-        <TextInput 
-          style={styles.textInput} 
-          placeholder='Enter your goal...' 
+        <TextInput
+          style={styles.textInput}
+          placeholder="Enter your goal..."
           onChangeText={goalInputHandler}
           value={enteredGoalText}
         />
-        <Button title='Add goal' onPress={addGoalHandler}/>
+        <Button title="Add goal" onPress={addGoalHandler} />
       </View>
       <View style={styles.goalsContainer}>
-        {courseGoals.map((goal, index) => (
-          <View key={index} style={styles.goalItem}>
-            <Text style={styles.goalItemText}>{goal}</Text>
-          </View>
-        ))}
+        <ScrollView>
+          {courseGoals.map((goal, index) => (
+            <View key={index} style={styles.goalItem}>
+              <Text style={styles.goalItemText}>{goal}</Text>
+            </View>
+          ))}
+        </ScrollView>
       </View>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   inputContainer: {
-    flex:1,
+    flex: 1,
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
@@ -51,16 +51,16 @@ const styles = StyleSheet.create({
     borderColor: 'black',
     width: '70%',
     marginRight: 8,
-    padding: 8
+    padding: 8,
   },
   goalsContainer: {
-   flex: 5, 
+    flex: 5,
   },
   goalItem: {
     margin: 8,
     padding: 8,
     backgroundColor: '#5e0acc',
-    borderRadius: 6
+    borderRadius: 6,
   },
   goalItemText: {
     color: 'white',
