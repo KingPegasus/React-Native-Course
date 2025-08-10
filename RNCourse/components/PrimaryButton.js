162dac872c1c3f58c9993a686905cc090dc1936f
Raza Saleemi <rks143@gmail.com>
2025-08-11 00:45:12 +0500

    MiniGame: Handling User Input & Showing an Alert Dialog

--- RNCourse/components/PrimaryButton.js
+++ RNCourse/components/PrimaryButton.js
@@ -1,13 +1,10 @@
 import { View, Text, Pressable, StyleSheet } from 'react-native';
 
-function PrimaryButton({ children }) {
-  function pressHandler() {
-    console.log('Button Pressed!');
-  }
+function PrimaryButton({ children, onPress }) {
   return (
     <View style={styles.buttonOuterContainer}>
       <Pressable
-        onPress={pressHandler}
+        onPress={onPress}
         android_ripple={{ color: '#500028' }}
         style={({ pressed }) =>
           pressed ? [styles.buttonInnerContainer, styles.pressed] : styles.buttonInnerContainer
