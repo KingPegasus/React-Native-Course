f6635a993b1a28c96de95343b18b885ef3e7a72a
Raza Saleemi <rks143@gmail.com>
2025-08-17 16:09:14 +0500

    Styling the Status Bar

--- RNCourse/App.js
+++ RNCourse/App.js
@@ -81,7 +81,7 @@ export default function App() {
       >
         <SafeAreaView style={styles.appContainer}>{screen}</SafeAreaView>
       </ImageBackground>
-      <StatusBar style="auto" />
+      <StatusBar style="light" />
     </LinearGradient>
   );
 }
--- RNCourse/screens/GameOver.js
+++ RNCourse/screens/GameOver.js
@@ -44,6 +44,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    padding: 24,
   },
   imageContainer: {
     borderRadius: 200,
