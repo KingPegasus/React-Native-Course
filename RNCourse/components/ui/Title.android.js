d782700cf955ce3268186785f84b0c74df59c4ff
Raza Saleemi <rks143@gmail.com>
2025-08-17 16:05:26 +0500

    Writing Platform-specific Code with the Platform API

--- RNCourse/components/ui/Title.android.js
+++ RNCourse/components/ui/Title.android.js
@@ -1,4 +1,4 @@
-import { Text, StyleSheet } from 'react-native';
+import { Text, StyleSheet, Platform } from 'react-native';
 
 function Title({ children }) {
   return <Text style={styles.title}>{children}</Text>;
@@ -14,6 +14,8 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     color: 'white',
     borderWidth: 2,
+    // borderWidth: Platform.OS === 'android' ? 2 : 0,
+    // borderWidth: Platform.select({ ios: 0, android: 2 }),
     borderColor: 'white',
     padding: 12,
     maxWidth: '80%',
