1fda475d863dc31df1d949c4b7e1a844435a3493
Raza Saleemi <rks143@gmail.com>
2025-08-22 07:31:25 +0500

    MealsApp: Displaying Meals

--- RNCourse/screens/MealsOverviewScreen.js
+++ RNCourse/screens/MealsOverviewScreen.js
@@ -1,15 +1,20 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { View, FlatList, StyleSheet } from 'react-native';
 
 import { MEALS } from '../data/dummy-data';
+import MealItem from '../components/MealItem';
 
 function MealsOverviewScreen({ route }) {
   const categoryId = route.params.categoryId;
   const meals = MEALS.filter(meal => meal.categoryIds.includes(categoryId));
+
+  function renderMealItem(itemData) {
+    const meal = itemData.item;
+    return <MealItem title={meal.title} />;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>
-        {categoryId} - {meals.length}
-      </Text>
+      <FlatList data={meals} keyExtractor={item => item.id} renderItem={renderMealItem} />
     </View>
   );
 }
